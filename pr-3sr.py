69e25855cf51bdfb4c0bdd61740134e0e6602e5e
Sumit Shishodia <140062213+s-82@users.noreply.github.com>
2024-10-21 15:29:58 +0530

    8/oct/2024

---
+++ pr-3sr.py
@@ -0,0 +1,13 @@
+n=int(input("enter num"))
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if j<=i:
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print()
+
+
+     
+
+
